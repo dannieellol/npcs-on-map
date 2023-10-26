57249ae5935551f0ebc1c28181cc120273644c51
Ludaro <waschmaschvanlu@gmail.com>
2023-10-26 15:41:19 +0200

    client.lua behoben und die variablen richtig eingetragen.

--- client.lua
+++ client.lua
@@ -1,41 +1,40 @@
 local isNearPed = false
 local isPedLoaded = false
-local pedModel = GetHashKey(Config.Model)
-local npc
-
 Citizen.CreateThread(function()
 
     while true do
-
+Citizen.Wait(1)
         local playerPed = PlayerPedId()
         local playerCoords = GetEntityCoords(playerPed)
-
-        local distance = Vdist(playerCoords, Config.NPCPosition.x, Config.NPCPosition.y, Config.NPCPosition.z)
-        isNearPed = false
+for k,v in pairs(Config.NPCS) do
+       -- local distance = Vdist(playerCoords, Config.NPCPosition.x, Config.NPCPosition.y, Config.NPCPosition.z) VDIST IST ALT UND SHER UNPERFORMANT MACH ES LIEBER WIE ICH
+       local distance = #(playerCoords, v.coords)
+       local isNearPed = false
 
         if distance < 30.0 then
-            isNearPed = true
+            local isNearPed = true
             if not isPedLoaded then
-                RequestModel(pedModel)
-                while not HasModelLoaded(pedModel) do
+                RequestModel(GetHashKey(v.model))
+                while not HasModelLoaded(GetHashKey(v.model)) do
                     Wait(10)
                 end
 
-                npc = CreatePed(4, pedModel, Config.NPCPosition.x, Config.NPCPosition.y, Config.NPCPosition.z - 1.0, Config.NPCPosition.rot, false, false)
+                npc = CreatePed(4, GetHashKey(v.model), v.coords.x, v.coords.y, v.coords.z - 1.0, v.coords.w, false, false)
                 FreezeEntityPosition(npc, true)
-                SetEntityHeading(npc, Config.NPCPosition.rot)
+                SetEntityHeading(npc, v.coords.w)
                 SetEntityInvincible(npc, true)
                 SetBlockingOfNonTemporaryEvents(npc, true)
 
-                isPedLoaded = true
+                local isPedLoaded = true
             end
         end
 
         if isPedLoaded and not isNearPed then
             DeleteEntity(npc)
             SetModelAsNoLongerNeeded(pedModel)
-            isPedLoaded = false
+            local isPedLoaded = false
         end
+end
         Citizen.Wait(500)
     end
 end)
--- config.lua
+++ config.lua
@@ -7,18 +7,12 @@ print "+---------------------------------------------+"
 	Config.NPCS = -- ein Table voll mit den NPCs
 	{
 	{
-		Config.Model = "a_m_m_salton_03", -- NPC1
-		Config.NPCPosition.x = -85.64,
-		Config.NPCPosition.y = 834.69,
-		Config.NPCPosition.z = 235.92,
-		Config.NPCPosition.rot = 99.21
+		model = "a_m_m_salton_03", -- NPC1
+		coords = vector4(-85.64, 834.69, 235.92, 99.21)
 	},
 		{
-		Config.Model = "a_m_m_salton_03", -- NPC2
-		Config.NPCPosition.x = 1557.2,
-		Config.NPCPosition.y = 3797.0,
-		Config.NPCPosition.z = 34.12,
-		Config.NPCPosition.rot = 202.04,
+		model = "a_m_m_salton_03", -- NPC2
+		coords = vector4(1557.2, 3793.0, 34.12, 202.04)
 	}
 }  
 
